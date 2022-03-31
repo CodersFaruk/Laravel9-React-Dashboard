00aa59bd0c046983f530550860cf08ff40924c43
CoderFaruk <66289933+CodersFaruk@users.noreply.github.com>
2022-03-31 09:33:36 +0600

    fixed bug react 18

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button} from 'react-bootstrap'
 
 function App() {
   return (
     <div>
       <h1>Ecommarce Dashboard</h1>
+      <Button variant="primary">Primary</Button>
+      <button>genaral</button>
     </div>
   );
 }
 
 export default App;
+
+
+
