85884705c09cbf6576d50acc691a7d772050e160
CoderFaruk <66289933+CodersFaruk@users.noreply.github.com>
2022-03-31 10:15:01 +0600

    Header section Route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
-import {Button} from 'react-bootstrap'
+import "./App.css";
+import { Button } from "react-bootstrap";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <h1>Ecommarce Dashboard</h1>
-      <Button variant="primary">Primary</Button>
-      <button>genaral</button>
+      <BrowserRouter>
+        <Header />
+        <Button variant="primary">Primary</Button>
+        <button>genaral</button>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
-
-
-
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Navbar,Nav,Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="/">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/addProduct">Add Product</Nav.Link>
+            <Nav.Link as={Link} to="/updatePorduct">Update Product</Nav.Link>
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+            <Nav.Link as={Link} to="/register">Register</Nav.Link>
+           
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
